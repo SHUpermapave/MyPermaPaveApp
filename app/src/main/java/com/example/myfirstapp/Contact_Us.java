54b05a6afd39d5d577cdac6e430ea806196918cb
abacall <b5021766@my.shu.ac.uk>
2017-01-10 09:51:30 +0000

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/Contact_Us.java
@@ -0,0 +1,55 @@
+package com.example.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Typeface;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.net.Uri;
+
+public class Contact_Us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+        Typeface buttonFont = Typeface.createFromAsset(getAssets(), "fonts/Cuprum-Regular.ttf");
+        Typeface textFont = Typeface.createFromAsset(getAssets(), "fonts/Arial.ttf");
+        Typeface boldFont = Typeface.createFromAsset(getAssets(), "fonts/Arial Bold.ttf");
+
+        TextView contactView = (TextView)findViewById(R.id.contact);
+        TextView address1View = (TextView)findViewById(R.id.address1);
+        TextView address2View = (TextView)findViewById(R.id.address2);
+        TextView phoneView = (TextView)findViewById(R.id.phone);
+        TextView emailView = (TextView)findViewById(R.id.email);
+        TextView bottomView = (TextView)findViewById(R.id.bottomBar);
+        Button backButton = (Button)findViewById(R.id.backButton);
+        Button sendButton = (Button)findViewById(R.id.sendButton);
+
+        contactView.setTypeface(boldFont);
+        address1View.setTypeface(textFont);
+        address2View.setTypeface(textFont);
+        phoneView.setTypeface(textFont);
+        emailView.setTypeface(textFont);
+        bottomView.setTypeface(textFont);
+        backButton.setTypeface(buttonFont);
+        sendButton.setTypeface(buttonFont);
+    }
+    public void back(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToMessageURL (View view) {
+        goToUrl ( "http://www.permapave.co.uk/contact-us");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Typeface buttonFont = Typeface.createFromAsset(getAssets(), "fonts/Cuprum-Regular.ttf");
+        Typeface textFont = Typeface.createFromAsset(getAssets(), "fonts/Arial.ttf");
+
+        Button visualizerButton = (Button)findViewById(R.id.selectButton);
+        Button requestButton = (Button)findViewById(R.id.requestButton);
+        Button tutorialButton = (Button)findViewById(R.id.tutorialButton);
+        Button contactButton = (Button)findViewById(R.id.contactButton);
+        TextView bottomView = (TextView)findViewById(R.id.bottomBar);
+
+        visualizerButton.setTypeface(buttonFont);
+        requestButton.setTypeface(buttonFont);
+        tutorialButton.setTypeface(buttonFont);
+        contactButton.setTypeface(buttonFont);
+        bottomView.setTypeface(textFont);
+    }
+    public void contact(View view) {
+        Intent intent = new Intent(this, Contact_Us.class);
+        startActivity(intent);
+    }
+    public void request(View view) {
+        Intent intent = new Intent(this, request_sample.class);
+        startActivity(intent);
+    }
+}
