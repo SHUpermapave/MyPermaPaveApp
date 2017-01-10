2ea8bab10c4e37af22e360dda95b8d0bb3e63976
Frank <b4003579@my.shu.ac.uk>
2017-01-10 14:23:50 +0000

    Updated top bar to grey Added hyperlink to bottom bar

--- app/src/main/java/com/example/myfirstapp/requestSample.java
+++ app/src/main/java/com/example/myfirstapp/requestSample.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -37,6 +38,14 @@ public class requestSample extends AppCompatActivity implements OnItemSelectedLi
         listViewColours.setOnItemSelectedListener(this);
 
         TextView bottomView = (TextView)findViewById(R.id.bottomBar);
+        bottomView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                goToWebsiteURL(v);
+            }
+        });
         Button backButton = (Button)findViewById(R.id.backButton);
         Button requestButton = (Button)findViewById(R.id.requestButton);
 
@@ -55,5 +64,15 @@ public class requestSample extends AppCompatActivity implements OnItemSelectedLi
     }
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void goToWebsiteURL (View view) {
+        goToUrl ( "http://www.permapave.co.uk");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
