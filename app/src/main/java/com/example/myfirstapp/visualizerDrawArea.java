6f385debfa2cc0da189ec3d0dac02665d3942a7e
RichGrimshaw <b5010083@my.shu.ac.uk>
2017-01-11 14:24:26 +0000

    draws line

--- app/src/main/java/com/example/myfirstapp/visualizerDrawArea.java
+++ app/src/main/java/com/example/myfirstapp/visualizerDrawArea.java
@@ -1,13 +1,10 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
-
-import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -33,7 +30,7 @@ public class visualizerDrawArea extends AppCompatActivity {
         Typeface buttonFont = Typeface.createFromAsset(getAssets(), "fonts/Cuprum-Regular.ttf");
         Typeface textFont = Typeface.createFromAsset(getAssets(), "fonts/Arial.ttf");
 
-        TextView connected = (TextView) findViewById(R.id.backButton);
+        TextView connected = (TextView) findViewById(R.id.connectedView);
         Button backButton = (Button)findViewById(R.id.backButton);
         Button selectButton = (Button)findViewById(R.id.selectButton);
 
