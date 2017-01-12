67743c0e7a2d05938d5b13ad9a1a0ca22b64eec8
abacall <b5021766@my.shu.ac.uk>
2017-01-12 14:05:53 +0000

    cfg

---
+++ app/src/test/java/com/example/myfirstapp/test1.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapp;
+
+/**
+ * Created by Aran on 12/01/2017.
+ */
+
+
+        import static org.junit.Assert.*;
+
+        import org.junit.Test;
+        import
+
+
+
+public class test1 {
+
+    @Test
+    public void visualize() {
+
+        assertTrue("Test Has passed ", );
+    }
+
+
+
+}
