8ffda3d3bdb356e150583f4a3bd34393642dd1cb
Zach Neocleous <b5030964@my.shu.ac.uk>
2017-01-13 11:25:32 +0000

    JUnit run Error

---
+++ app/src/test/java/com/example/myfirstapp/test5.java
@@ -0,0 +1,14 @@
+package com.example.myfirstapp;
+
+import junit.framework.*;
+import android.test.ViewAsserts.*;
+import org.junit.Test;
+
+public class test5  extends TestCase{
+
+    @Test
+    public void testButton ()
+    {
+        assertNotNull("requestSample.class");
+    }
+}
